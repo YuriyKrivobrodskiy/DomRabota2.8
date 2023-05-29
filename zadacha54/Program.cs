ccd36c6d06b79afecd51ca7b535a122165f6cac0
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-05-29 20:23:06 +0300

    zadacha 54

--- zadacha54/Program.cs
+++ zadacha54/Program.cs
@@ -1,12 +1,4 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
 
 int[,] table = new int[3, 4];
 FillArrayRandom(table);
@@ -16,7 +8,6 @@ Console.WriteLine();
 PrintArray(table);
 
 
-// Функция заполнения массива рандомно числами от 1 до 9
 void FillArrayRandom(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -28,7 +19,6 @@ void FillArrayRandom(int[,] array)
     }
 }
 
-// Функция сортировки элементов в строке двумерного массива, по убыванию
 void SortToLower(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -48,7 +38,6 @@ void SortToLower(int[,] array)
     }
 }
 
-// Функция вывода двумерного массива
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
